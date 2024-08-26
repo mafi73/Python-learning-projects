6a551dfd14adff8fda32884201b1e44cdc581d11
Mohammadreza <m.mafi73@gmail.com>
2024-08-26 22:10:00 +0330

    just a simple exercise for finding even and odd numbers using the Python programming language.

---
+++ 01zojvafard.py
@@ -0,0 +1,12 @@
+#peyda kardane addae zoj va fard va amnfi va sefr
+x=input('adad vared konid? ')
+x=int(x)
+if x>0:
+    if x%2==0:
+         print('zooj')
+    else:
+         print('fard')
+elif x==0:
+     print('adad sefr ast')
+elif x<0:
+     print('adad manfi ast')
