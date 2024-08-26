11fefffd825ff39320fd183ca6ea9d8c9e159695
Mohammadreza <m.mafi73@gmail.com>
2024-08-26 23:16:02 +0330

    A simple program for identifying names and characters using conditional statements.

---
+++ 02esm ha.py
@@ -0,0 +1,9 @@
+x=input('esme khod ra vared konid? ')
+if x=='mohammad':
+     print('salam mohammad')
+elif x==('jadi'):
+     print('ee jadi toam injaiii??')
+elif x==('fateme'):
+     print('salam abji')
+else:
+     print('salam gharibe')
