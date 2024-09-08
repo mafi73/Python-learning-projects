3154c5abf79cc27e2e0a1f421e855544a3036f76
Mohammadreza <m.mafi73@gmail.com>
2024-09-08 21:28:46 +0330

    Valid Parentheses

---
+++ Valid-Parentheses.py
@@ -0,0 +1,19 @@
+s=input('please give a string? ')
+list=[]
+for i in s:
+    if i in '([{':
+        list.append(i)
+    elif not list:
+        print ('False')
+    elif i==')':
+        if list.pop() != '(':
+            print ('False')
+    elif i=='}':
+        if list.pop() != '{':
+            print ('False')
+    elif i==']':
+        if list.pop() != '[':
+            print ('False')
+if list:
+    print ('False')
+print (True)
