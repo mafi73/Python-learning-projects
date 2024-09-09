90f5bffd8b7605163d118de7cd1b83bb4954ff1c
Mohammadreza <m.mafi73@gmail.com>
2024-09-10 01:11:36 +0330

    convert to function mode.

--- LenthOfLastWord.py
+++ LenthOfLastWord.py
@@ -1,6 +1,12 @@
 #leetcode problems
 #Given a string s consisting of words and spaces,
 #  return the length of the last word in the string.
-str=input('give me a string? ')
-l=str.split()
-print (len(l[-1]))
+def length_of_last_word(s):
+    words = s.split()
+    if words:
+        return len(words[-1])
+    return 0
+
+# مثال استفاده
+input_str = input('give me a string? ')
+print(length_of_last_word(input_str))
