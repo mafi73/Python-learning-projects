664ca43b19f844d3838648e1a579513c613161d3
Mohammadreza <m.mafi73@gmail.com>
2024-09-10 23:58:00 +0330

    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.

--- Merge_Sorted_Array.py
+++ Merge_Sorted_Array.py
@@ -1,9 +1,9 @@
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
+nums1 = [1,2,3,5,0,0,0,0]
+m = 4
+nums2 = [2,5,6,0,0]
 n = 3
-nums1=nums1+nums2
-for i in nums1:
-    if i==0:
-        nums1.remove(i)
+nums1=(nums1+nums2)
+nums1.sort()
+n=n+m
+nums1=nums1[-n:]
 print(nums1)        
