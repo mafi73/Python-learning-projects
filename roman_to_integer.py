f35b356bb69d90e574ef7003f545c616369fdf49
Mohammadreza <m.mafi73@gmail.com>
2024-09-13 22:14:06 +0330

    bugfix

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,4 +1,4 @@
-s="III"
+s="XL"
 convertor={
 'I':'1',"V":"5","X":"10","L":"50",
 "C":"100","D":"500","M":"1000",
@@ -7,6 +7,20 @@ convertor={
 "XL":"40","XC":"90",
 "CD":"400","CM":"900"}
 answer=0
+if "XL" in s:
+    s.replace('XL','')
+    answer+=40
+if "XC" in s:
+    s.replace('XC','')
+    answer+=90
+if "CD" in s:
+    s.replace('CD','')
+    answer+=400
+if "CM" in s:
+    s.replace('CM','')
+    answer+=900
+
 for i in s:
+
     answer+= int(convertor[i])
 print (answer)     
