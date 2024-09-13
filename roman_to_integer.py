32783fd9d8dd9f025e1c1dcb2ed34d2e3b512594
Mohammadreza <m.mafi73@gmail.com>
2024-09-13 22:23:34 +0330

    [200~Given a roman numeral, convert it to an integer.

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,23 +1,30 @@
-s="XL"
+#Given a roman numeral, convert it to an integer.
+s="LVIII"
 convertor={
-'I':'1',"V":"5","X":"10","L":"50",
+"I":"1","V":"5","X":"10","L":"50",
 "C":"100","D":"500","M":"1000",
 "IV":"4",
 "IX":"9",
 "XL":"40","XC":"90",
 "CD":"400","CM":"900"}
 answer=0
+if "IV" in s:
+    s=s.replace("IV","")
+    answer+=4
+if "IX" in s:
+    s=s.replace('IX','')
+    answer+=9
 if "XL" in s:
-    s.replace('XL','')
+    s=s.replace('XL','')
     answer+=40
 if "XC" in s:
-    s.replace('XC','')
+    s=s.replace('XC','')
     answer+=90
 if "CD" in s:
-    s.replace('CD','')
+    s=s.replace('CD','')
     answer+=400
 if "CM" in s:
-    s.replace('CM','')
+    s=s.replace('CM','')
     answer+=900
 
 for i in s:
