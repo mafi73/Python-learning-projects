45773e5a8cbf340b53b8cb2bb036b2ba85aa96ad
Mohammadreza <m.mafi73@gmail.com>
2024-09-15 13:22:25 +0330

    Remove Element from list-bug fix

--- Remove_element.py
+++ Remove_element.py
@@ -1,4 +1,4 @@
-#Remove Element
+#Remove Element from list
 #https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 
 l=[1,2,2,3,6,3,4,5,8,5,4,5,6]
