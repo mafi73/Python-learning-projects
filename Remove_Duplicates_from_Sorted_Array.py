1a1b1f6ed2521f2443398e6bfd5a5338aaa4b5da
Mohammadreza <m.mafi73@gmail.com>
2024-09-15 11:06:08 +0330

    #Remove Duplicates from Sorted Array

--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -1,12 +1,14 @@
-array=[1,2,2,3,3,3,4,6,6,6,6]
+#Remove Duplicates from Sorted Array
+array=[1,1,2,2,3,3,3,4,6]
 i=0
 c=0
-while i<len(array):
+while (i+1)<len(array):
     if array[i]==array[i+1]:
-        array.pop(array[i])
-        i+=1
+        array.remove(array[i])
+        #OR--> array.pop(i)  # Remove the element at index i
+
         c+=1
     else:
         i+=1
-print(array)
-print(c)
+print(array) #print array without duplicate
+print(c) #numbers of duplicated numbers
