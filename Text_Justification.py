354d925dffb5f182a70f6f4f425ac22e52d238b6
Mohammadreza <m.mafi73@gmail.com>
2024-09-23 01:50:27 +0330

    Given an array of strings words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.-bugfix

--- Text_Justification.py
+++ Text_Justification.py
@@ -1,6 +1,6 @@
 #Leetcode Problems
 #https://leetcode.com/problems/text-justification/description/
-list=["This", "is", "an", "example", "of", "text", "justification."]
+list=["What","must","be","acknowledgment","shall","be"]
 maxWidth=16
 lines=[]#خطوط نهایی
 current_line=[] #کلمات خط فعلی
@@ -12,14 +12,13 @@ for w in list:
     else:
         fazaha = maxWidth - current_lenght #koole fazahaye khali
         if len(current_line)==1: #تک کلمه ای
-            line=current_line[0]+(fazaha*' ')#
+            strline=current_line[0]+(fazaha * ' ')#
         else:
             fazaye_vasat = fazaha // (len(current_line)-1)#tedade faza bein kalamat
-            space_namosavi=fazaha % (len(current_line)-1)#adad
-            space= fazaye_vasat*' ' #tedad space ha
+            space_namosavi=fazaha % (len(current_line)-1)#adad         
             strline=''
             for i in range(len(current_line)-1):
-                strline += current_line[i]
+                strline += current_line[i] #kalame ezaf kardan
                 if i < space_namosavi:
                     strline += ' ' *((fazaye_vasat)+1)#ezafe kardan faza ezafe be samte chap tar
                 else:
@@ -28,6 +27,7 @@ for w in list:
         lines.append(strline)
         current_lenght=len(w)
         current_line=[w]
-final_line=' '.join(current_line)+ ' '*(maxWidth - len(' '.join(current_line)))
+final_line=' '.join(current_line) 
+final_line+=' ' * (maxWidth- len(final_line)) #chap chin kardan khate akhar va por kardane samte rast ba space
 lines.append(final_line)
 print (lines)
