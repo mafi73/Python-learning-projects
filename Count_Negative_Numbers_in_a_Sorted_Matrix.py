a5a3bdaa7a02d621659fa941453e12ff87a74143
Mohammadreza <m.mafi73@gmail.com>
2024-09-23 21:03:55 +0330

    Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.

--- Count_Negative_Numbers_in_a_Sorted_Matrix.py
+++ Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -1,3 +1,6 @@
+#Leetcode 
+#https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
+#Count Negative Numbers in a Sorted Matrix
 matrix=[[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 c=0
 for i in matrix:
