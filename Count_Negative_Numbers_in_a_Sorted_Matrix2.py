24cebcb384dc6132fdf3d930b86f0bf22daf55b2
Mohammadreza <m.mafi73@gmail.com>
2024-09-25 02:14:51 +0330

    Optimal solution to obtain negative numbers in a sorted matrix_bugfix

--- Count_Negative_Numbers_in_a_Sorted_Matrix2.py
+++ Count_Negative_Numbers_in_a_Sorted_Matrix2.py
@@ -7,13 +7,12 @@ column = len(matrix[0]) #tedade sotoonha
 c = 0
 prow = 0 # shroro az satr 0(avval) sotone akhar (bala samt rast)
 pcolumn = column - 1 #satr avval sotone akhar
-while (prow < row) and (pcolumn) >= 0:
+while (prow < row) and (pcolumn >= 0):
     if matrix[prow][pcolumn] < 0: #satrha ta paiin manfi an
         c += (row - prow) #az oun satr be paiin hamash manfi ast
         pcolumn -= 1  # harekat be sotone ghabl
     else:
-        prow += 1  # satre paiin check mishe
-
+        prow += 1  # satre paiin check mishet 
 print(c)
 
 
